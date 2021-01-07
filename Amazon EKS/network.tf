c27bfcb5ab735468e53fd68bd1ff32dcef1c6990
Antonio Thomacelli <tonnytg@gmail.com>
2021-01-07 07:12:03 -0300

    Add comment about DNS resolution

--- Amazon EKS/network.tf
+++ Amazon EKS/network.tf
@@ -9,8 +9,8 @@
 # Create a VPC of K8S
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
+  enable_dns_hostnames  = true # by default come disable except default VPC
+  enable_dns_support    = true # by default come disable except default VPC
   tags = {
       Name = "myVPC"
   }
