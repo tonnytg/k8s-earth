a0fc9ea97cf57a4a6e628ba6c1d942549e558ada
Antonio Thomacelli <tonnytg@gmail.com>
2021-01-07 07:33:32 -0300

    public subnet and internet gateway created

--- Amazon EKS/network.tf
+++ Amazon EKS/network.tf
@@ -7,23 +7,118 @@
 # https://docs.aws.amazon.com/eks/latest/userguide/eks-networking.html
 
 # Create a VPC of K8S
-resource "aws_vpc" "myvpc" {
+resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames  = true # by default come disable except default VPC
   enable_dns_support    = true # by default come true
   tags = {
-      Name = "myVPC"
+      Name = format("%s-vpc",var.cluster_name)
   }
 }
 
-# Create a Public Subnet for K8S
-resource "aws_subnet" "public_subnet_1a" {
+####
+# Public Subnet
+###
+# Create a Public Subnet A for K8S
+resource "aws_subnet" "eks_subnet_public_1a" {
     vpc_id = aws_vpc.myvpc.id
 
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "10.0.10.0/24"
     map_public_ip_on_launch = true
     availability_zone = format("%sa", var.aws_region)
+
+    # TAG alert
+    # https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html#vpc-subnet-tagging
     tags = {
-        Name = "public"
+        Name = format("%s-eks_subnet_public_1a", var.cluster_name)
+        "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     }
 }
+
+# Create a Public Subnet B for K8S
+resource "aws_subnet" "eks_subnet_public_1b" {
+    vpc_id = aws_vpc.myvpc.id
+
+    cidr_block = "10.0.11.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = format("%sb", var.aws_region)
+
+    # TAG alert
+    # https://docs.aws.amazon.com/eks/latest/userguide/network_reqs.html#vpc-subnet-tagging
+    tags = {
+        Name = format("%s-eks_subnet_public_1b", var.cluster_name)
+        "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    }
+}
+
+# Edit Route Table of Subnet Public A
+resource "aws_route_table_association" "eks_public_rt_association_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.eks_public_rt.id # Link with Internet Gateway
+}
+
+# Edit Route Table of Subnet Public B
+resource "aws_route_table_association" "eks_public_rt_association_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.eks_public_rt.id # Link with Internet Gateway
+}
+
+
+####
+# Private Subnet
+###
+# Create a Private Subnet A for K8S
+resource "aws_subnet" "eks_subnet_private_1a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = format("%sa", var.region)
+
+  tags = {
+    Name = format("%s-subnet-private-1a", var.cluster_name)
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
+# Create a Private Subnet B for K8S
+resource "aws_subnet" "eks_subnet_private_1b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.21.0/24"
+  availability_zone = format("%sb", var.region)
+
+  tags = {
+    Name = format("%s-subnet-private-1b", var.cluster_name)
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
+# Edit Route Table of Subnet Private A
+resource "aws_route_table_association" "eks_private_rt_association_1a" {
+  subnet_id      = aws_subnet.eks_subnet_private_1a.id
+  route_table_id = aws_route_table.eks_nat_rt.id
+}
+# Edit Route Table of Subnet Private A
+resource "aws_route_table_association" "eks_private_rt_association_1b" {
+  subnet_id      = aws_subnet.eks_subnet_private_1b.id
+  route_table_id = aws_route_table.eks_nat_rt.id
+}
+
+###
+# Internet Gateway
+###
+# Internet Gateway allow Public Subnet to access Internet
+resource "aws_internet_gateway" "eks_ig" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = format("%s-internet-gateway", var.cluster_name)
+  }
+}
+# Add route 0.0.0.0/0 to in and out
+resource "aws_route_table" "eks_public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_ig.id
+  }
+  tags = {
+    Name = format("%s-public-rt", var.cluster_name)
+  }
+}
+
