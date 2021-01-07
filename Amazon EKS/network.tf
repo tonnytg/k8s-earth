d82009cdc038f23509b781e75374b0031944533e
Antonio Thomacelli <tonnytg@gmail.com>
2021-01-07 07:15:33 -0300

    fix comment about DNS resolution

--- Amazon EKS/network.tf
+++ Amazon EKS/network.tf
@@ -10,7 +10,7 @@
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames  = true # by default come disable except default VPC
-  enable_dns_support    = true # by default come disable except default VPC
+  enable_dns_support    = true # by default come true
   tags = {
       Name = "myVPC"
   }
