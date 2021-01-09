b677c0f1c12c30d008199f2e20647262660b04bc
Antonio Thomacelli <tonnytg@gmail.com>
2021-01-09 16:46:17 -0300

    variables command eksctl edited

--- Amazon EKS/variables.tf
+++ Amazon EKS/variables.tf
@@ -9,3 +9,9 @@ variable "cluster_name" {
   type    = string
   default = "myEKS"
 }
+
+# Message to help build EKS
+variable "eks_command" {
+  type    = string
+  default = "eksctl create cluster --name myCluster --version 1.18 --region us-east-1 --managed --nodes 2 --nodes-min 1 --nodes-max 3 --vpc-private-subnets=<SUBNET PRIVATE ID 1>,<SUBNET PRIVATE ID 1> --vpc-public-subnets=<SUBNET PUBLIC ID 1>,<SUBNET PUBLIC ID 2>"
+}
